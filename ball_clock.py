226d7c5d4f5af2e0d88a0a34988fee7dcee95d8f
Jeremy Cornett <jeremy.cornett@venafi.com>
2017-06-09 22:42:53 -0600

    I didn't realize that the criteria for input is just in and out. The requirements don't want command line parameters.

--- ball_clock.py
+++ ball_clock.py
@@ -13,20 +13,28 @@ def main(balls):
     :type balls: int
     :return: None
     """
-    validate_input(balls)
-
     print "I'm a ball machine with {} balls and I have no idea how many days I can keep " \
           "track of time, yet.".format(balls)
 
 
-def validate_input(ball_count):
+def valid_input(the_input):
     """Validate that the ball count is a known valid number.
-    :param ball_count: The number of balls in the ball clock machine.
-    :type ball_count: int
-    :return: None
+    :param the_input: Hopefully he number of balls in the ball clock machine.
+    :type the_input: str
+    :return: If the input was valid (True) or not (False).
+    :rtype: bool
     """
+    try:
+        ball_count = int(the_input)
+    except:
+        print "ValueError: '{}' is not valid input. Integers between 27 and 127 only. 0 to exit.".format(the_input)
+        return False
+    if ball_count == 0:
+        return True
     if ball_count < 27 or 127 < ball_count:
-        raise ValueError("The number of balls ({}) to test must be between 27 and 127.".format(ball_count))
+        print "ValueError: The number of balls ({}) to test must be between 27 and 127. 0 to exit.".format(ball_count)
+        return False
+    return True
 
 
 def test_example_1():
@@ -40,21 +48,11 @@ def test_example_2():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Given a number of balls, how many days can the ball clock keep '
-                                                 'track of time?')
-    parser.add_argument('balls', metavar='N', type=int, nargs='*',
-                        help='The number of balls in the ball clock (between 27 and 127). '
-                             'You can enter multiple numbers.')
-    args = parser.parse_args()
-    if args.balls is None or len(args.balls) == 0:
-        main(-1)
-    list_errors = []
-    for a_number in args.balls:
-        try:
-            main(a_number)
-        except ValueError as err:
-            list_errors.append(err)
-
-    for an_error in list_errors:
-        print "ValueError: {}".format(an_error)
-    sys.exit(len(list_errors))
+    while True:
+        user_input = raw_input()
+        if not valid_input(user_input):
+            continue
+        ball_count = int(user_input)
+        if ball_count == 0:
+            break
+        main(ball_count)
